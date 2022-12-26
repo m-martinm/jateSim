6fad936c54a4ccf958f0b2604389ede34627f941
Martin <malkocs.martin@gmail.com>
2022-12-26 03:15:43 +0100

    up

--- jateSim/src/Main.java
+++ jateSim/src/Main.java
@@ -1,10 +1,11 @@
 import app.Engine;
 
+// build https://stackoverflow.com/questions/60788566/how-do-i-make-an-executable-file-from-an-intellij-idea-project
 public class Main
 {
   public static void main(String[] args)
   {
 
-    Engine test = new Engine(600, 600);
+    new Engine(800, 800);
   }
 }
